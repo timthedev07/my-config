704ba25a2e1aa263fbe68a12d8f13a7de72bac48
Tim <harvard90873@gmail.com>
2021-09-26 16:54:21 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,21 @@ set -gx LDFLAGS /usr/local/opt/ruby/lib
 set -gx CPPFLAGS /usr/local/opt/ruby/include
 set -gx PKG_CONFIG_PATH /usr/local/opt/ruby/lib/pkgconfig
 status --is-interactive; and source (rbenv init -|psub)
+alias simulator='open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
+alias python='python3'
+alias hideDesktopIcons='defaults write com.apple.finder CreateDesktop false && killall Finder'
+alias showDesktopIcons='defaults write com.apple.finder CreateDesktop true && killall Finder'
 
 function reEvaluateDataGrip
   rm -rf ~/Library/Application\ Support/JetBrains/DataGrip2021.1/eval/*
 end
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/highsierra/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+# Setting PATH for Python 3.9
+# The original version is saved in /Users/highsierra/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.9/bin" "$PATH"
+
+
